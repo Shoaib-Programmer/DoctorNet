9c266781adff35f9a7cfffd1ba1c193b46473399
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-07-26 15:03:39 +0000

    Added shadcn and the hero component from 21st.dev

---
+++ src/components/ui/hero.tsx
@@ -0,0 +1,39 @@
+import { MoveRight, PhoneCall } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+function Hero() {
+  return (
+    <div className="w-full py-20 lg:py-40">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-8 items-center md:grid-cols-2">
+          <div className="flex gap-4 flex-col">
+            <div className="flex gap-4 flex-col">
+              <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-regular">
+                DoctorNet
+              </h1>
+              <p className="text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left">
+                Managing a small business today is already tough. Avoid further
+                complications by ditching outdated, tedious trade methods.
+              </p>
+            </div>
+            <div className="flex flex-row gap-4">
+              <Button size="lg" className="gap-4" variant="outline">
+                Jump on a call <PhoneCall className="w-4 h-4" />
+              </Button>
+              <Button size="lg" className="gap-4">
+                Sign up here <MoveRight className="w-4 h-4" />
+              </Button>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-8">
+            <div className="bg-muted rounded-md aspect-square"></div>
+            <div className="bg-muted rounded-md row-span-2"></div>
+            <div className="bg-muted rounded-md aspect-square"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Hero };
